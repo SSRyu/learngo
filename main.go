e66f2de8d29eef7b4b286db7c3a1a3a4ac2cf10f
SSRyu <jjkztok3@gmail.com>
2021-08-19 22:25:46 +0900

    Go routine and channel

--- main.go
+++ main.go
@@ -1,18 +1,48 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"learngo/accounts"
+	"net/http"
 )
 
+var (
+	errRequestFailed = errors.New("Request failed")
+)
+
+type requestResult struct {
+	url    string
+	status string
+}
+
 func main() {
-	account := accounts.NewAccount("niko")
-	account.Deposit(100)
-	fmt.Println(account)
-	err := account.Withdraw(200)
-	if err != nil {
-		// log.Fatalln(err)
-		fmt.Println(err)
+	c := make(chan requestResult)
+	results := make(map[string]string)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	fmt.Println(account)
+
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+}
+
+func hitURL(url string, c chan<- requestResult) {
+	resp, _ := http.Get(url)
+	c <- requestResult{url: url, status: resp.Status}
 }
