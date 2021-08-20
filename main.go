8291a670617280712b727113449a1ef0526511f2
SSRyu <jjkztok3@gmail.com>
2021-08-20 22:32:38 +0900

    Add getter

--- main.go
+++ main.go
@@ -1,48 +1,96 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
+
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
 
 var (
-	errRequestFailed = errors.New("Request failed")
+	query   string = "python"
+	limit   int    = 50
+	baseURL string = fmt.Sprintf("https://jp.indeed.com/jobs?q=%s&limit=%d&", query, limit)
 )
 
-type requestResult struct {
-	url    string
-	status string
-}
-
 func main() {
-	c := make(chan requestResult)
-	results := make(map[string]string)
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
-	}
-
-	for _, url := range urls {
-		go hitURL(url, c)
-	}
-	for i := 0; i < len(urls); i++ {
-		result := <-c
-		results[result.url] = result.status
-	}
-	for url, status := range results {
-		fmt.Println(url, status)
+	// fmt.Println(baseURL)
+	totalPages := getPages()
+	fmt.Println(totalPages)
+	// for i := 0; i < totalPages; i++ {
+	for i := 0; i < 1; i++ {
+		getPage(i)
 	}
 }
 
-func hitURL(url string, c chan<- requestResult) {
-	resp, _ := http.Get(url)
-	c <- requestResult{url: url, status: resp.Status}
+func getPage(page int) {
+	pageURL := fmt.Sprint(baseURL, "start=", limit*page)
+	fmt.Println(pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".jobsearch-SerpJobCard")
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		id, _ := card.Attr("data-jk")
+		title := strings.TrimSpace(card.Find(".title>a").Text())
+		location := strings.TrimSpace(card.Find(".sjcl").Text())
+		fmt.Println(i, id, title, location)
+	})
+}
+
+func getPages() int {
+	pages := 0
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	pageRegexp := regexp.MustCompile(`[0-9,]+`)
+	doc.Find("#searchCountPages").Each(func(i int, s *goquery.Selection) {
+		fmt.Println()
+		for _, str := range strings.Split(strings.TrimSpace(s.Text()), " ") {
+			matched := pageRegexp.MatchString(str)
+			if matched {
+				num, _ := strconv.Atoi(strings.ReplaceAll(str, ",", ""))
+				pages = (num / 50) + 1
+				break
+			}
+		}
+	})
+
+	return pages
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status:", res.Status)
+	}
 }
